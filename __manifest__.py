2230134a10023121cc9224b8d7a191c5bf65e12e
SebastianBetancurCastillo <sbetancur@pragmaticingenieria.com>
2023-10-03 11:46:10 -0500

    Comienzo Página Pragmatic

---
+++ __manifest__.py
@@ -0,0 +1,32 @@
+{
+   'name': 'Theme Pragmatic',
+   'description': 'Tema Pragmatic Ingeniería',
+   'category': 'Theme/Website',
+   'version': '16.0',
+   'author': 'Pragmatic Ingeniería',
+   'depends': ['web','website'],
+   'data': [
+        'views/pages/home.xml',
+        'views/pages/contactus.xml',
+        'views/pages/404.xml',
+        'views/pages/about_us.xml',
+        'views/menu.xml',
+        'views/website_templates.xml',
+        'views/presets.xml'
+    ],
+
+
+    'assets': {
+        'web.assets_frontend': [
+        'theme_pragmatic/static/src/scss/website_templates.scss',
+        'theme_pragmatic/static/src/scss/style_home.scss'
+        ],
+    },
+
+
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+
+
+}
