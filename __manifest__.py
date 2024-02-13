910b23154be19a01df43a587e58d2b4b4881467f
adgiraldog <agiraldo@pragmaticingenieria.com>
2024-02-13 10:45:55 -0500

    signin-responsive

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,17 @@
 {
-   'name': 'Theme Pragmatic',
-   'description': 'Tema Pragmatic Ingeniería',
-   'category': 'Theme/Website',
-   'version': '16.0',
-   'author': 'Pragmatic Ingeniería',
-   'depends': ['web','website'],
-   'data': [
+    'name': 'Theme Pragmatic',
+    'description': 'Tema Pragmatic Ingeniería',
+    'category': 'Theme/Website',
+    'version': '16.0',
+    'author': 'Pragmatic Ingeniería',
+    'depends': ['web', 'website'],
+    'data': [
         'views/pages/home.xml',
         'views/pages/contactus.xml',
         'views/pages/blog.xml',
         'views/pages/404.xml',
         'views/pages/about_us.xml',
+        'views/pages/signin.xml',
         'views/pages/software_erp.xml',
         'views/pages/software_crm.xml',
         'views/pages/consultorias.xml',
@@ -21,21 +22,19 @@
         'views/presets.xml'
     ],
 
-
     'assets': {
         'web.assets_frontend': [
-        'theme_pragmatic/static/src/scss/website_templates.scss',
-        'theme_pragmatic/static/src/scss/style_home.scss',
-        'theme_pragmatic/static/src/scss/style_contact.scss',
-        'theme_pragmatic/static/src/scss/style_blog.scss',
-        'theme_pragmatic/static/src/scss/style_service.scss',
+            'theme_pragmatic/static/src/scss/website_templates.scss',
+            'theme_pragmatic/static/src/scss/style_home.scss',
+            'theme_pragmatic/static/src/scss/style_contact.scss',
+            'theme_pragmatic/static/src/scss/style_blog.scss',
+            'theme_pragmatic/static/src/scss/style_service.scss',
+            'theme_pragmatic/static/src/scss/signin.scss',
         ],
     },
-
 
     'installable': True,
     'auto_install': False,
     'application': True,
-
 
 }
